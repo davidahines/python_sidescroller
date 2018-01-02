e0ddc4eb38f1810e5dbe7f98b4054634774066ac
David Hines <david.hines.01@gmail.com>
2018-01-02 00:02:09 -0400

    Added some sprites. Changed the map a bit to add stairs. Starting writing support for stairs.

--- main.py
+++ main.py
@@ -21,9 +21,7 @@ from pyscroll.group import PyscrollGroup
 # define configuration variables here
 RESOURCES_DIR = 'data'
 
-HERO_JUMP_HEIGHT = 180
-HERO_MOVE_SPEED = 200  # pixels per second
-GRAVITY = 1000
+
 MAP_FILENAME = 'maps/dungeon_0.tmx'
 
 
@@ -67,13 +65,33 @@ class Hero(pygame.sprite.Sprite):
     def __init__(self, map_data_object):
         pygame.sprite.Sprite.__init__(self)
 
+        # CONSTANTS
+        self.HERO_JUMP_HEIGHT = 180
+        self.HERO_MOVE_SPEED = 200  # pixels per second
+        self.GRAVITY = 1000
+
+        # STATE DEFINITIONS
         self.STATE_STANDING = 0
         self.STATE_WALKING = 1
         self.STATE_JUMPING = 2
+        self.STATE_ON_STAIRS = 3
+        self.STATE_CROUCHED = 4
 
-        self.FRAME_DELAY_STANDING =1
+        self.FRAME_DELAY_STANDING = 1
         self.FRAME_DELAY_WALKING = 1
         self.FRAME_DELAY_JUMPING = 1
+
+        # ANIMATION - Lower is slower
+        self.ANIMATION_SPEED_WALKING = .25
+        self.ANIMATION_SPEED_STANDING = .002
+
+        self.JUMP_DELAY =.3
+
+        self.CLIMBING_DELAY = .3
+        self.CLIMBING_SPEED = 200
+
+        self.CLIMBING_UP = 0
+        self.CLIMBING_DOWN = 1
 
         self.FACING_RIGHT = 0
         self.FACING_LEFT = 1
@@ -83,6 +101,9 @@ class Hero(pygame.sprite.Sprite):
         self.COLLISION_BOX_OFFSET = 8
 
 
+        self.climbing_direction = 0
+        self.time_spent_climbing = 0.0
+        self.time_since_last_jump = 0
         self.time_in_state = 0.0
         self.current_walking_frame = 0
         self.current_standing_frame = 0
@@ -100,6 +121,14 @@ class Hero(pygame.sprite.Sprite):
             self.state = state
             self.time_in_state = 0.0
 
+    def snap_to_stair(self, dt, game):
+        stair_sensor = self.get_stair_sensor()
+        for stair in game.stairs:
+            if stair_sensor.colliderect(stair.rect):
+                self.rect.bottom = stair.rect.top
+                self.set_state(self.STATE_ON_STAIRS)
+                self._position[1] = self.rect.top
+                break
 
     def load_sprites(self):
         self.spritesheet = Spritesheet('data/art/platformer_template_g.png')
@@ -113,15 +142,27 @@ class Hero(pygame.sprite.Sprite):
             ), colorkey= (0,255,81))
 
         walk_images = self.spritesheet.images_at((
-            pygame.Rect(128, 0, 32, 32),pygame.Rect(160, 0, 32, 32),
-            pygame.Rect(192, 0, 32, 32),pygame.Rect(224, 0, 32, 32),
-            pygame.Rect(0, 0, 32, 32),pygame.Rect(32, 0, 32, 32),
-            pygame.Rect(64, 0, 32, 32),pygame.Rect(96, 0, 32, 32),
-            ), colorkey= (0,255,81))
+
+            pygame.Rect(192, 0, 32, 32),
+            pygame.Rect(96, 0, 32, 32),
+            pygame.Rect(64, 32, 32, 32),
+            pygame.Rect(96, 0, 32, 32),
+
+        ), colorkey= (0,255,81))
 
         jumping_images = self.spritesheet.images_at((
             pygame.Rect(160, 160, 32, 32),
             ), colorkey= (0,255,81))
+
+        stairs_descending_images = self.spritesheet.images_at((
+            pygame.Rect(32, 224, 32, 32),
+            pygame.Rect(128, 224, 32, 32),
+            ), colorkey= (0,255,81))
+
+        stairs_ascending_images = self.spritesheet.images_at((
+            pygame.Rect(32, 192, 32, 32),
+            pygame.Rect(128, 192, 32, 32),
+        ), colorkey=(0, 255, 81))
 
         self.walk_images = []
         for walk_image in walk_images:
@@ -136,6 +177,13 @@ class Hero(pygame.sprite.Sprite):
         for jumping_image in jumping_images:
             self.jumping_images.append(jumping_image.convert_alpha())
 
+        self.stairs_descending_images = []
+        for stairs_descending_image in stairs_descending_images:
+            self.stairs_descending_images.append(stairs_descending_image.convert_alpha())
+
+        self.stairs_ascending_images = []
+        for stairs_ascending_image in stairs_ascending_images:
+            self.stairs_ascending_images.append(stairs_ascending_image.convert_alpha())
 
         self.image = self.standing_images[self.current_standing_frame]
 
@@ -149,6 +197,10 @@ class Hero(pygame.sprite.Sprite):
 
     def get_floor_sensor(self):
         return pygame.Rect(self.position[0]+self.COLLISION_BOX_OFFSET, self.position[1]+2, self.rect.width -self.COLLISION_BOX_OFFSET, self.rect.height)
+
+    def get_stair_sensor(self):
+        return pygame.Rect(self.position[0]+self.COLLISION_BOX_OFFSET, self.position[1]+self.rect.height-2, self.rect.width -self.COLLISION_BOX_OFFSET, 4)
+
 
     def get_ceiling_sensor(self):
         return pygame.Rect(self.position[0]+self.COLLISION_BOX_OFFSET, self.position[1]-self.rect.height, self.rect.width, 2)
@@ -171,30 +223,27 @@ class Hero(pygame.sprite.Sprite):
             else:
                 if game.debug:
                     print("increasing downward velocity: {}".format(self.velocity[1]))
-                self.velocity[1] += GRAVITY * dt
+                self.velocity[1] += self.GRAVITY * dt
                 if game.debug:
                     print("is now: {}".format(self.velocity[1]))
 
     def animate(self, dt, game):
-        if(self.state == self.STATE_WALKING):
-            #if self.time_in_state > self.FRAME_DELAY_WALKING:
-            self.current_walking_frame  = int(self.time_in_state * self.MILLISECONDS_TO_SECONDS % len(self.walk_images))
+        if self.state == self.STATE_WALKING:
+            self.current_walking_frame  = int(self.time_in_state * self.MILLISECONDS_TO_SECONDS * self.ANIMATION_SPEED_WALKING % len(self.walk_images))
             self.image = self.walk_images[self.current_walking_frame]
             self.time_in_state += dt
 
             if game.debug:
                 print("Walking frame: {}".format(self.current_walking_frame))
-        elif(self.state == self.STATE_STANDING):
-            #if self.time_in_state > self.FRAME_DELAY_STANDING:
-            self.current_standing_frame  = int(self.time_in_state * self.MILLISECONDS_TO_SECONDS % len(self.standing_images))
+        elif self.state == self.STATE_STANDING:
+            self.current_standing_frame  = int(self.time_in_state * self.MILLISECONDS_TO_SECONDS * self.ANIMATION_SPEED_STANDING % len(self.standing_images))
             self.image = self.standing_images[self.current_standing_frame]
             self.time_in_state += dt
             if game.debug:
                 print("time_standing: {}".format(self.time_in_state))
                 print("len(walk_images){}".format(len(self.walk_images)))
                 print("Standing frame: {}".format(self.current_standing_frame))
-        elif(self.state == self.STATE_JUMPING):
-            #if self.time_in_state > self.FRAME_DELAY_STANDING:
+        elif self.state == self.STATE_JUMPING:
             self.current_jump_frame = int(self.time_in_state * self.MILLISECONDS_TO_SECONDS % len(self.jumping_images))
             self.image = self.jumping_images[self.current_jump_frame]
             self.time_in_state += dt
@@ -202,6 +251,25 @@ class Hero(pygame.sprite.Sprite):
                 print("time_standing: {}".format(self.time_in_state))
                 print("len(jump_images){}".format(len(self.jumping_images)))
                 print("Jumping")
+        elif self.state == self.STATE_ON_STAIRS:
+            if self.velocity[1] > 0:
+                self.current_desc_frame = int(self.time_spent_climbing * self.MILLISECONDS_TO_SECONDS % len(self.stairs_descending_images))
+
+                self.image = self.stairs_descending_images[self.current_desc_frame]
+                self.time_in_state += dt
+                if game.debug:
+                    print("time_spent_climbing: {}".format(self.time_spent_climbing))
+                    print("len(jump_images){}".format(len(self.jumping_images)))
+                    print("Jumping")
+            else:
+                self.current_asc_frame = int(self.time_spent_climbing * self.MILLISECONDS_TO_SECONDS % len(self.stairs_ascending_images))
+
+                self.image = self.stairs_ascending_images[self.current_asc_frame]
+                self.time_in_state += dt
+                if game.debug:
+                    print("time_spent_climbing: {}".format(self.time_spent_climbing))
+                    print("len(jump_images){}".format(len(self.jumping_images)))
+                    print("Jumping")
         else:
             if game.debug:
                 print("state is {}".format(self.state))
@@ -212,6 +280,7 @@ class Hero(pygame.sprite.Sprite):
             self.image = pygame.transform.flip(self.image,True,False)
 
     def update(self, dt, game):
+        self.time_since_last_jump += dt
         self.calc_grav(game, dt)
         self.animate(dt, game)
 
@@ -221,12 +290,21 @@ class Hero(pygame.sprite.Sprite):
 
     def move(self, dt, game):
         # Move each axis separately. Note that this checks for collisions both times.
-        dx = self.velocity[0]
-        dy = self.velocity[1]
-        if dx != 0:
-            self.move_single_axis(dx, 0, dt)
-        if dy != 0:
-            self.move_single_axis(0, dy, dt)
+        if self.state == self.STATE_ON_STAIRS:
+            if self.time_in_state > self.CLIMBING_DELAY:
+                if self.time_spent_climbing > self.CLIMBING_SPEED / 2:
+                    positions_to_move = Math.floor(self.time_spent_climbing / self.CLIMBING_SPEED)
+                    self.time_spent_climbing = self.time_spent_climbing % self.CLIMBING_SPEED
+                    self._position[0] += dt * self.CLIMBING_SPEED * positions_to_move
+                    self._position[1] += dt * self.velocity[1] * positions_to_move
+                    print("positions_to_move: ".format(self.positions_to_move))
+        else:
+            dx = self.velocity[0]
+            dy = self.velocity[1]
+            if dx != 0:
+                self.move_single_axis(dx, 0, dt)
+            if dy != 0:
+                self.move_single_axis(0, dy, dt)
         self.rect.topleft = self._position
     def move_single_axis(self, dx, dy, dt):
         #print("hero_destination: ({}, {})".format(dx *dt, dy *dt))
@@ -240,21 +318,19 @@ class Hero(pygame.sprite.Sprite):
         body_sensor = self.get_body_sensor()
         for wall in game.walls:
             if body_sensor.colliderect(wall.rect):
-                if dx > 0: # Moving right; Hit the left side of the wall
-                    #print(" -- Moving right; Hit the left side of the wall")
+                if dx > 0:  # Moving right; Hit the left side of the wall
                     self.rect.right = wall.rect.left
-                if dx < 0: # Moving left; Hit the right side of the wall
-                    #print(" -- Moving left; Hit the right side of the wall")
+                    self._position[0] = self.rect.left
+                if dx < 0:  # Moving left; Hit the right side of the wall
                     self.rect.left = wall.rect.right - self.COLLISION_BOX_OFFSET
-                if dy > 0: # Moving down; Hit the top side of the wall
-                    #print(" -- Moving down; Hit the top side of the wall")
+                    self._position[0] = self.rect.left
+                if dy > 0:  # Moving down; Hit the top side of the wall
                     self.rect.bottom = wall.rect.top
-                if dy < 0: # Moving up; Hit the bottom side of the wall
-                    #print(" -- Moving up; Hit the bottom side of the wall")
+                    self._position[1] = self.rect.top
+                if dy < 0:  # Moving up; Hit the bottom side of the wall
                     self.rect.top = wall.rect.bottom
+                    self._position[1] = self.rect.top
 
-        self._position[0] = self.rect.topleft[0]
-        self._position[1] = self.rect.topleft[1]
 
 
 
@@ -333,9 +409,12 @@ class QuestGame(object):
         # setup level geometry with simple pygame rects, loaded from pytmx
         self.walls = list()
         self.npcs = list()
+        self.stairs = list()
         for map_object in self.tmx_data.objects:
             if map_object.type == "wall":
                 self.walls.append(Wall(map_object))
+            elif map_object.type == "stair":
+                self.stairs.append(Wall(map_object))
             elif map_object.type == "guard":
                 print("npc load failed: reimplement npc")
                 #self.npcs.append(Npc(map_object))
@@ -344,10 +423,6 @@ class QuestGame(object):
 
         # create new data source for pyscroll
         map_data = pyscroll.data.TiledMapData(self.tmx_data)
-
-
-
-
 
         # create new renderer (camera)
         self.map_layer = pyscroll.BufferedRenderer(map_data, screen.get_size(), clamp_camera=True, tall_sprites=1)
@@ -377,10 +452,10 @@ class QuestGame(object):
 
 
         if(self.debug):
-            floor_sensor_rect = self.hero.get_floor_sensor()
+            floor_sensor_rect = self.hero.get_stair_sensor()
 
             ox, oy = self.map_layer.get_center_offset()
-            new_rect = floor_sensor_rect.move(ox * 2, oy * 2)
+            new_rect = floor_sensor_rect.move(ox, oy)
 
             pygame.draw.rect(surface, (255,0,0), new_rect)
 
@@ -412,8 +487,8 @@ class QuestGame(object):
 
 
         floor_sensor = self.hero.get_floor_sensor()
-        floor_collidelist = floor_sensor.collidelist(self.walls)
-        hero_is_airborne = floor_collidelist == -1
+        floor_wall_collidelist = floor_sensor.collidelist(self.walls)
+        hero_is_airborne = floor_wall_collidelist == -1
 
 
         ceiling_sensor = self.hero.get_ceiling_sensor()
@@ -428,31 +503,51 @@ class QuestGame(object):
             print("hero position: {}, {}".format(self.hero.position[0], self.hero.position[1]))
             print("hero_touches_ceiling: {}".format(hero_touches_ceiling))
             print("hero_is_airborne: {}".format(hero_is_airborne))
-        if hero_is_airborne == False:
+            print("hero_state: {}".format(self.hero.state))
+        if pressed[K_UP] and not pressed[K_DOWN]:
+            if self.hero.state == self.hero.STATE_ON_STAIRS:
+                if self.hero.FACING_RIGHT:
+                    self.hero.velocity[0] = self.hero.CLIMBING_SPEED
+                else:
+                    self.hero.velocity[0] = -self.hero.CLIMBING_SPEED
+                self.hero.velocity[1] = -self.hero.CLIMBING_SPEED
+            else:
+                self.hero.snap_to_stair(dt, self)
+        if pressed[K_DOWN] and not pressed[K_UP]:
+            if self.hero.state == self.hero.STATE_ON_STAIRS:
+                if self.hero.FACING_RIGHT:
+                    self.hero.velocity[0] = self.hero.CLIMBING_SPEED
+                else:
+                    self.hero.velocity[0] = -self.hero.CLIMBING_SPEED
+                self.hero.velocity[1] = self.hero.CLIMBING_SPEED
+            else:
+                self.hero.snap_to_stair(dt, self)
+        if hero_is_airborne == False and self.hero.state != self.hero.STATE_ON_STAIRS:
 
             #JUMP
-            if pressed[K_SPACE]:
+            if pressed[K_SPACE] and self.hero.time_since_last_jump >= self.hero.JUMP_DELAY:
                 self.hero.set_state(self.hero.STATE_JUMPING)
 
                 # stop the player animation
                 if pressed[K_LEFT] and pressed[K_RIGHT] == False:
                     # play the jump left animations
-                    self.hero.velocity[0] = -HERO_MOVE_SPEED
+                    self.hero.velocity[0] = -self.hero.HERO_MOVE_SPEED
                     self.hero.facing = self.hero.FACING_LEFT
                     jumped_direction = 0
                 elif pressed[K_RIGHT] and pressed[K_LEFT] == False:
-                    self.hero.velocity[0] = HERO_MOVE_SPEED
+                    self.hero.velocity[0] = self.hero.HERO_MOVE_SPEED
                     self.hero.facing = self.hero.FACING_RIGHT
                     jumped_direction = 1
-                self.hero.velocity[1]= -HERO_JUMP_HEIGHT
+                self.hero.velocity[1]= -self.hero.HERO_JUMP_HEIGHT
+                self.hero.time_since_last_jump = 0
             elif pressed[K_LEFT] and pressed[K_RIGHT] == False:
                 self.hero.set_state(self.hero.STATE_WALKING)
                 self.hero.facing = self.hero.FACING_LEFT
-                self.hero.velocity[0] = -HERO_MOVE_SPEED
+                self.hero.velocity[0] = -self.hero.HERO_MOVE_SPEED
             elif pressed[K_RIGHT] and pressed[K_LEFT] == False:
                 self.hero.set_state(self.hero.STATE_WALKING)
                 self.hero.facing = self.hero.FACING_RIGHT
-                self.hero.velocity[0] = HERO_MOVE_SPEED
+                self.hero.velocity[0] = self.hero.HERO_MOVE_SPEED
             else:
                 self.hero.state = self.hero.STATE_STANDING
                 self.hero.velocity[0] = 0
